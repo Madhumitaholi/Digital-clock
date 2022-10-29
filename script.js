4270f6cb70bceb970370c49e65727ebc7937d82e
MADHUMITA KUNDU <88303234+Madhumitaholi@users.noreply.github.com>
2022-10-30 03:13:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+// let age=18;
+// switch(age){
+//     case (age<18):
+//     console.log("child");
+//     break;
+//     case (18<age<40):
+//         console.log("Adult");
+//     default:
+//         console.log("old age");
+// }
+let n=2;
+switch(n){
+    case (0) :
+        console.log("Today is Monday");
+        break;
+    case (1) :
+        console.log("Today is Tuesday");
+        break;
+    case (2) :
+            console.log("Today is wednesday");
+            break;
+    case (3) :
+        console.log("Today is Thursday");
+        break;
+    case (4) :
+            console.log("Today is Friday");
+            break;
+    case (5) :
+        console.log("Today is Saturday");
+        break;
+    case (6) :
+            console.log("Today is Sunday");
+        break;
+    default:
+        console.log("No weekdays");
+}
